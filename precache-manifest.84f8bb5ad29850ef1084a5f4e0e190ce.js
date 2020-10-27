a31a21648414c55cd3a19ae2e4031e2dd843302b
vgandzyuck <v.gandzyuck@aisnovations.com>
2020-10-27 11:11:43 +0300

    Updates

---
+++ precache-manifest.84f8bb5ad29850ef1084a5f4e0e190ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f54cc936c2fa2c736eee844806da20a2",
+    "url": "/pokemons/index.html"
+  },
+  {
+    "revision": "681f6aa8e1a5a16e2d1c",
+    "url": "/pokemons/static/css/main.9daeae9c.chunk.css"
+  },
+  {
+    "revision": "e5ab94fb1c9544955287",
+    "url": "/pokemons/static/js/2.7a36e855.chunk.js"
+  },
+  {
+    "revision": "8da57ccee1690fd1d0790b5fcb8f1bad",
+    "url": "/pokemons/static/js/2.7a36e855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681f6aa8e1a5a16e2d1c",
+    "url": "/pokemons/static/js/main.3a63db53.chunk.js"
+  },
+  {
+    "revision": "691f0e6aacd424fec7fc",
+    "url": "/pokemons/static/js/runtime-main.d08412cb.js"
+  }
+]);
