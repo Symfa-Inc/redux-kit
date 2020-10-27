ca7adac9f5f7e9b771adadfeed8423bd24857172
vgandzyuck <v.gandzyuck@aisnovations.com>
2020-10-27 11:55:11 +0300

    Updates

---
+++ precache-manifest.7c262cf617d3bf7bf7cc09a61e12feb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9327d242f20bc4aa409e29c7d4f22113",
+    "url": "/redux-kit/index.html"
+  },
+  {
+    "revision": "681f6aa8e1a5a16e2d1c",
+    "url": "/redux-kit/static/css/main.9daeae9c.chunk.css"
+  },
+  {
+    "revision": "e5ab94fb1c9544955287",
+    "url": "/redux-kit/static/js/2.7a36e855.chunk.js"
+  },
+  {
+    "revision": "8da57ccee1690fd1d0790b5fcb8f1bad",
+    "url": "/redux-kit/static/js/2.7a36e855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "681f6aa8e1a5a16e2d1c",
+    "url": "/redux-kit/static/js/main.3a63db53.chunk.js"
+  },
+  {
+    "revision": "4dffa773fd8db265466a",
+    "url": "/redux-kit/static/js/runtime-main.39c87341.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pokemons/precache-manifest.84f8bb5ad29850ef1084a5f4e0e190ce.js"
+  "/redux-kit/precache-manifest.7c262cf617d3bf7bf7cc09a61e12feb9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokemons/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/redux-kit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
